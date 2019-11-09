e08229acdb1b315f2ea067473bfc84ddbd9cf098
Tomek Rabczak <tomek.rabczak@gmail.com>
2019-11-09 09:51:25 -0600

    Highlight chord notes in a scale

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Synth } from "tone";
-import Fretboard, { chordNotes } from 'react-fretboard'
+import Fretboard, { chordNotes, scaleNotes } from 'react-fretboard'
+import { Chord } from "tonal";
 import './App.css';
 
-//create a synth and connect it to the master output (your speakers)
-//var synth = new Tone.Synth().toMaster();
-//
-////play a middle 'C' for the duration of an 8th note
-//synth.triggerAttackRelease("C4", "8n");
+// react-fretboard helper to generate a list of fretboard note objects that can
+// be passed to the selectedNotes property of the Fretboard component
+const fretboardNotesInScale = scaleNotes('C', 'major');
+
+// Use the Tonal library to generate the list of chord notes in the scale
+const chordNotesInScale = Chord.notes('CM');
+
+// Mark chord notes with a status of "chordNote" so they can be highlighted in
+// green 
+const fretboardNotes = fretboardNotesInScale.map(fretboardNote => {
+  if (chordNotesInScale.includes(fretboardNote.note)) {
+    fretboardNote.status = "chordNote";
+  }
+  return fretboardNote
+});
 
 function App() {
   return (
@@ -15,7 +26,8 @@ function App() {
       <div>
         <Fretboard
           skinType="strings"
-          selectedNotes={['C3', 'E3', 'G3']}
+          selectedNotes={fretboardNotes}
+          theme={{ statusMap: { chordNote: '#b9e59e' }}}
         />
       </div>
     </div>
